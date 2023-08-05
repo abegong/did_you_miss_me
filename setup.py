7223f4bcfee1e02bfe852dcf8afd9dd8228c4ea3
Abe Gong <abegong@gmail.com>
2023-08-05 08:37:57 -0600

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='missing_data_generator',
+    version='0.1.0',
+    install_requires=[
+        'faker',
+        'pandas',
+        'pydantic',
+    ],
+    extras_require={
+        'dev': ['pytest'],
+    },
+    packages=find_packages(include=['missing_data_generator', 'missing_data_generator.*'])
+)
